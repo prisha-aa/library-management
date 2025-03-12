79b2fe0e3bda44ad5593957910d343cdb576cd9f
Prisha <prishabhasin14@gmail.com>
2025-03-12 11:07:33 +0300

    onfigured placeholder images

---
+++ components/BookCover.tsx
@@ -0,0 +1,38 @@
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import React from 'react'
+type BookCOverVariant="extraSmall"| "small" | "medium" | "regular" | "wide";
+
+const variantStyles:Record<BookCoverVariant,string>={
+    extraSmall:'book-cover_extra_small',
+    small:'book-cover_extra_small',
+    medium:'book-cover_extra_small',
+    regular:'book-cover_extra_small',
+    wide:'book-cover_extra_small',
+}
+
+interface Props{
+    classname?:string;
+    variant: BookCoverVariant;
+    coverCOlor:string;
+    coverUrl:string;
+}
+
+const BookCover = ({
+    className,
+    variant="regular",
+    coverColor='#012848',
+    coverUrl="https://placehold.co/400x600.png"}:Props) => {
+  return (
+    <div className={cn('relative transition-all duration-300',variantStyles[variant],
+        className,
+    )}>
+      BOOK SIDE SVG
+      <div className="absolute z-10" style={{ left: "12%", width: "87.5%", height: "88%" }}>
+        <Image src={coverUrl} alt="Book cover" fill className='rounded-sm object-fill'
+      </div>
+    </div>
+  )
+}
+
+export default BookCover
