1c3250091b74655b72337fcd4dc47f00226e2ad5
Prisha <prishabhasin14@gmail.com>
2025-03-12 11:50:43 +0300

    done with hero section

--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -1,15 +1,17 @@
 import { cn } from '@/lib/utils';
 import Image from 'next/image';
 import React from 'react'
+import BookCoverSvg from './BookCoverSvg';
 type BookCoverVariant="extraSmall"| "small" | "medium" | "regular" | "wide";
 
-const variantStyles:Record<BookCoverVariant,string>={
-    extraSmall:'book-cover_extra_small',
-    small:'book-cover_extra_small',
-    medium:'book-cover_extra_small',
-    regular:'book-cover_extra_small',
-    wide:'book-cover_extra_small',
-}
+const variantStyles: Record<BookCoverVariant, string> = {
+  extraSmall: 'book-cover_extra_small',
+  small: 'book-cover_small',
+  medium: 'book-cover_medium',
+  regular: 'book-cover_regular',
+  wide: 'book-cover_wide',
+};
+
 
 interface Props{
     classname?:string;
@@ -27,10 +29,12 @@ const BookCover = ({
 
   return (
     <div 
-    className={cn('relative transition-all duration-300',variantStyles[variant],
+    className={cn('relative transition-all duration-300',
+      variantStyles[variant],
         className,
     )}>
-       <BookCoverSvg coverColor={coverColor} />
+       <BookCoverSvg coverColor={coverColor}/>
+
       <div className="absolute z-10" style={{ left: "12%", width: "87.5%", height: "88%" }}>
         <Image 
         src={coverImage} 
--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -11,8 +11,9 @@ const BookOverview = ({
   total_copies,
   available_copies,
    description,
-   color,
-    cover,}:Book) => {
+   coverColor,
+    coverUrl,}:Book) => {
+      
   return (
     <section className='book-overview'>
       <div className='flex flex-1 flex-col gap-5'>
@@ -53,15 +54,15 @@ const BookOverview = ({
           <BookCover
           variant="wide"
           className="z=10"
-          coverColor={color}
-          coverImage={cover}
+          coverColor={coverColor}
+          coverImage={coverUrl}
           />
 
           <div className="absolute left-16 top-10 rotate-12 opacity-40 max-sm:hidden">
           <BookCover
           variant="wide"
-          coverColor={color}
-          coverImage={cover}
+          coverColor={coverColor}
+          coverImage={coverUrl}
           />
           </div>
 
