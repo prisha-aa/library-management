af3527514d67dee7ffa9256410dc4f43dfc745b3
Prisha <prishabhasin14@gmail.com>
2025-03-17 00:19:20 +0530

    added routes for imagekit authentication

--- app/api/auth/imagekit/route.ts
+++ app/api/auth/imagekit/route.ts
@@ -1,3 +1,15 @@
 import ImageKit from "imagekit";
+import config from "@/lib/config";
+import { NextResponse } from "next/server";
 
-const imagekit= new ImageKit();
+const {
+  env: {
+    imagekit: { publicKey, privateKey, urlEndpoint },
+  },
+} = config;
+
+const imagekit = new ImageKit({ publicKey, privateKey, urlEndpoint });
+
+export async function GET() {
+  return NextResponse.json(imagekit.getAuthenticationParameters());
+}
