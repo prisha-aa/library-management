776e72effef0140e713fafd1aa64cb7175cd7b4d
Prisha <prishabhasin14@gmail.com>
2025-03-21 18:40:12 +0530

    database migration done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,17 @@
 import BookList from '@/components/BookList'
 import BookOverview from '@/components/BookOverview'
 import { sampleBooks } from '@/constants'
+import { db } from '@/database/drizzle'
+import { users } from '@/database/schema'
 import React from 'react'
 
-const Home = () => {
+const Home =async () => {
+
+  const result= await db.select().from(users);
+  console.log(JSON.stringify(result,null,2))
+
   return (
-    <div>
+    <>
       <BookOverview {...sampleBooks[0]}/>
 
       <BookList
@@ -14,7 +20,7 @@ const Home = () => {
       containerClassName="mt-28"
       />
       
-    </div>
+    </>
   )
 }
 
