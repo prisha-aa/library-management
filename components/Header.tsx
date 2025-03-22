b882a8eab231884ffda5d1c426c7bdba8b1a5e72
Prisha <prishabhasin14@gmail.com>
2025-03-22 23:45:03 +0530

    added my profile section in the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,13 @@
 "use client";
-import { cn } from '@/lib/utils';
+import { cn, getInitials } from '@/lib/utils';
 import  Image  from 'next/image';
 import Link from 'next/link'
 import { usePathname } from 'next/navigation';
 import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
-const Header = () => {
+
+const Header = ({session:{session}}) => {
     const pathname=usePathname();
 
 
@@ -24,8 +26,24 @@ const Header = () => {
                     pathname==='/library' ? 'text-light-200':"text-light-100",
 
                 )}>
-            Library</Link>
+            Library
+            </Link>
             </li>
+
+            <li>
+                <Link href="/my-profile">
+                <Avatar>
+                
+                <AvatarFallback className='bg-amber-100'>
+                    {getInitials(session?.user?.name || "IN")}
+
+                </AvatarFallback>
+                </Avatar>
+
+
+                </Link>
+            </li>
+
 
 
         </ul>
